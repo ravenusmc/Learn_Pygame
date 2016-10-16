2c9624b4d93783f3e8b879a62205418ed70fd214
Mike Cuddy <mcuddy77@gmail.com>
2016-10-16 07:02:07 -0400

    added quit function

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import pygame
 
 pygame.init()
 
+white = (255,255,255)
+
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Slither')
 
-pygame.display.update()
 gameExit = False
 
 while not gameExit:
     for event in pygame.event.get():
-        print(event)
-        
-
-
-
+        if event.type == pygame.QUIT:
+            gameExit = True
+            
 pygame.quit()
 quit()
+
+            
+        
+
