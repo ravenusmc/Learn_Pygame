9fa8920dd4eae2373e32e8b7ac69b18678f759e3
Mike Cuddy <mcuddy77@gmail.com>
2016-10-16 07:21:23 -0400

    added ability to move square

--- main.py
+++ main.py
@@ -11,13 +11,23 @@ pygame.display.set_caption('Slither')
 
 gameExit = False
 
+lead_x = 300
+lead_y = 300
+
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x -= 10
+            if event.key == pygame.K_RIGHT:
+                lead_x += 10
 
 
     gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y, 10, 10])
+    
     pygame.display.update()
             
 pygame.quit()
