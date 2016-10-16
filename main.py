31300ac70d30386aa3164d53d3e34abaa05dcad4
Mike Cuddy <mcuddy77@gmail.com>
2016-10-16 09:11:02 -0400

    added up down movement

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ gameExit = False
 lead_x = 300
 lead_y = 300
 lead_x_change = 0
+lead_y_change = 0
 
 clock = pygame.time.Clock()
 
@@ -24,16 +25,20 @@ while not gameExit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x_change = -10
-            if event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
                 lead_x_change = 10
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_UP:
+                lead_y_change = -10
+                lead_x_change = 0
+            elif event.key == pygame.K_DOWN:
+                lead_y_change = 10
                 lead_x_change = 0
             
 
     lead_x += lead_x_change
-
+    lead_y += lead_y_change
     gameDisplay.fill(white)
     pygame.draw.rect(gameDisplay, black, [lead_x,lead_y, 10, 10])
     pygame.display.update()
